acde217eb66a7481ec9cd1aa99e293820a2c27be
Re-Dei <hridaya.pradhan123@gmail.com>
2023-03-30 10:03:53 +0545

    added inputs for email and password and checkboxes

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,20 +1,87 @@
+'use client'
 //Home page
 import styles from './page.module.css'
+import { useState } from 'react';
+// import { createClient } from '@supabase/supabase-js'
+
+// const supabase = createClient('https://xyzcompany.supabase.co', 'public-anon-key')
 
 export default function Home() {
   return (
-    <main>
-      <Login/>
-    </main>
+    <div>
+      <div className={ styles.bg }></div>
+      <div className={ styles.container }>
+        <Login/>
+      </div>
+    </div>
   )
 }
 
 
 function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
-    <div>
-      <>Login</>
-      <input></input>
-    </div>
+    <>
+      <text className={styles.header_login}>Login</text>
+
+      <input 
+        placeholder='Email Address or Mobile Number' 
+        className={ styles.input_login_email } 
+        type='email'
+      />
+      
+      <input 
+        placeholder='Password' 
+        className={ styles.input_login_password } 
+        type={ showPassword? 'text': 'password' }
+      />
+
+      <input 
+      type='checkbox' 
+      id='show_password'
+      onChange={ () => setShowPassword((showPassword) => !showPassword) }
+      />
+      <label htmlFor='show_password'>Show Password</label>
+      <br />
+
+      <input 
+      type='checkbox' 
+      id='keep_me_signed_in'
+      // onChange={}
+      />
+      <label htmlFor='keep_me_signed_in'>Keep Me Signed In</label>
+
+      <br />
+      <a target='_blank'>Forgot Password?</a>
+
+      <button>Sign In</button>
+      {/* <button onClick={ () => signInWithGoogle }>Sign In with Google</button> */}
+    </>
   )
 }
+
+// async function signInWithGoogle() {
+//   const { data, error } = await supabase.auth.signInWithOAuth({
+//     provider: 'google',
+//   })
+// }
+
+// async function signout() {
+//   const { error } = await supabase.auth.signOut()
+// }
+
+/** for Google OAuth 2.0 
+ * Google OAuth2.0 doesn't return the provider_refresh_token by default. 
+ * If you need the provider_refresh_token returned, you will need to add additional query parameters: */
+// async function signInWithGoogle() {
+//   const { data, error } = await supabase.auth.signInWithOAuth({
+//     provider: 'google',
+//     options: {
+//       queryParams: {
+//         access_type: 'offline',
+//         prompt: 'consent',
+//       },
+//     },
+//   })
+// }
