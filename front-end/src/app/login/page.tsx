580674084d6d338f854bc7832cacc1697c342e1f
BigyanByanju <90769738+BigyanByanju@users.noreply.github.com>
2023-04-05 17:40:19 +0545

    front-end done

--- front-end/src/app/login/page.tsx
+++ front-end/src/app/login/page.tsx
@@ -1,86 +1,118 @@
-'use client'
+"use client";
 //Home page
-import styles from './page.module.css'
-import { useState } from 'react';
-import {FcGoogle} from "react-icons/fc";
-import {SiFacebook} from "react-icons/si";
-
-import { useRouter } from 'next/navigation'
-import { signIn } from 'next-auth/react'
-import Link from 'next/link';
+import loginStyles from "./page.module.css";
+import { useState } from "react";
+import { FcGoogle } from "react-icons/fc";
+import { SiFacebook } from "react-icons/si";
+import bgImg from "../../../public/ncpr.jpg";
+import { useRouter } from "next/navigation";
+import { signIn } from "next-auth/react";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className={ styles.App }>
+    <div className={loginStyles.app}>
       <Login />
-    </div> 
-  )
+    </div>
+  );
 }
-
 
 function Login() {
-  const [showPassword, setShowPassword] = useState(false)
-  const [email, setEmail] = useState<string>("")
-  const [password, setPassword] = useState<string>("")
+  const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
-  const handleSignIn = async() => {
-    const res = await signIn('credentials', { email, password, redirect: true, callbackUrl: '/feeds' })
-    console.log(res)
-  }
-
+  const handleSignIn = async () => {
+    const res = await signIn("credentials", {
+      email,
+      password,
+      redirect: true,
+      callbackUrl: "/feeds",
+    });
+    console.log(res);
+  };
   return (
-    <div className= { styles.logform }>
-      <text className={ styles.header_login }>Login</text>
-      
-      <input 
-      value={ email }
-      placeholder='Email Address or Mobile Number' 
-      className={ styles.input } 
-      type='email'
-      onChange={ (e) => setEmail(e.target.value) }
-      />
-        
-        <input 
-          value={ password }
-          placeholder='Password' 
-          className={ styles.input }
-          type={ showPassword? 'text': 'password' }
-          onChange={ (e) => setPassword(e.target.value) }
-        />
+    <div className={loginStyles.wrapper}>
+      <div className={loginStyles.imgBox}>
+        <img src={bgImg} alt="backgroundImage" />
+      </div>
 
-        <input 
-        type='checkbox' 
-        id='show_password'
-        className={ styles.check_signin }
-        onChange={ () => setShowPassword((showPassword) => !showPassword) }
-        />
-        <label htmlFor='show_password'>Show Password</label>
-        <br />
+      <div className={loginStyles.loginBox}>
+        <div className={loginStyles.formBox}>
+          <h2>Login</h2>
 
-        <input 
-        type='checkbox' 
-        id='keep_me_signed_in'
-        className={ styles.check_signin }
-        // onChange={}
-        />
-        <label htmlFor='keep_me_signed_in'>Keep Me Signed In</label>
+          <form>
+            <input
+              value={email}
+              placeholder="Email Address or Mobile Number"
+              className={loginStyles.inputBx}
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
 
-        <br />
-        {/* <Link href='/reset_password'>Forgot Password?</Link> */}
+            <br />
 
-        <button 
-        className={ styles.signin_btn }
-        onClick={ handleSignIn }>Sign In</button>
-        {/* <button onClick={ signInWithGoogle }>Sign In with Google</button> */}
-        <br />
-        <>Don't have an account?</>
-        <Link href='/sign_up'>Sign Up</Link>
+            <input
+              value={password}
+              placeholder="Password"
+              className={loginStyles.inputBx}
+              type={showPassword ? "text" : "password"}
+              onChange={(e) => setPassword(e.target.value)}
+            />
 
-        <div className={ styles.alt_login }>
-          <button className={ styles.alt_btn }>Continue with google <FcGoogle className='icon'/></button>
-          <button className={ styles.alt_btn }>Continue with facebook <SiFacebook className='icon'/></button>
-        </div> 
+            <br />
+            <div className={loginStyles.chkCtn}>
+              <div className={loginStyles.showCtn}>
+                <input
+                  type="checkbox"
+                  id="show_password"
+                  className={loginStyles.check}
+                  onChange={() =>
+                    setShowPassword((showPassword) => !showPassword)
+                  }
+                />
+                <label htmlFor="show_password">Show Password</label>
+              </div>
+
+              <div className={loginStyles.remCtn}>
+                <input
+                  type="checkbox"
+                  id="keep_me_signed_in"
+                  className={loginStyles.check}
+                  // onChange={}
+                />
+                <label htmlFor="keep_me_signed_in">Remember me</label>
+              </div>
+            </div>
+
+            {/* <Link href='/reset_password'>Forgot Password?</Link> */}
+
+            <br />
+
+            <button className={loginStyles.Sbtn} onClick={handleSignIn}>
+              Sign In
+            </button>
+            {/* <button onClick={ signInWithGoogle }>Sign In with Google</button> */}
+
+            <br />
+            <div className={loginStyles.dText}>
+              <>Don't have an account?&nbsp; </>
+              <Link href="/sign_up"> Sign Up</Link>
+            </div>
+
+            <div className={loginStyles.AbtnCtn}>
+              <button className={loginStyles.Abtn}>
+                Continue with google &nbsp;{" "}
+                <FcGoogle className={loginStyles.icon} />
+              </button>
+              <button className={loginStyles.Abtn}>
+                Continue with facebook &nbsp;
+                <SiFacebook className={loginStyles.icon} />
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
-
