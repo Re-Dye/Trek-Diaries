172d1af2e096cdbb0c48f5631e44632f2fbf2a81
Re-Dei <hridaya.pradhan123@gmail.com>
2023-04-05 12:26:30 +0545

    merged ui in login

--- front-end/src/pages/api/auth/[...nextauth].ts
+++ front-end/src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,4 @@
-import NextAuth, { NextAuthOptions, User } from "next-auth";
+import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 export default NextAuth({
