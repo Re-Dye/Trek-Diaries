3570f26b857b50f1ec8965db2f3ffc7059b4b275
Re-Dei <hridaya.pradhan123@gmail.com>
2023-04-20 20:32:34 +0545

    fixed typescript error

--- front-end/lib/mongoose.ts
+++ front-end/lib/mongoose.ts
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI;
+declare global {
+  var mongoose: any;
+}
+
+const MONGODB_URI = process.env.MONGODB_URI as string;
 
 if (!MONGODB_URI) {
   throw new Error(
