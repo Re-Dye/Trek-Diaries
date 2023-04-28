1e8828c5ac312b57981032d1ee593a3851fc1d78
Re-Dei <hridaya.pradhan123@gmail.com>
2023-04-28 15:10:27 +0545

    added fix for session in layout

--- front-end/src/app/(site)/layout.tsx
+++ front-end/src/app/(site)/layout.tsx
@@ -1,29 +1,14 @@
-'use client'
+/* eslint-disable @next/next/no-head-element */
 
-import { Session } from 'next-auth';
-import './globals.css'
-import { SessionProvider } from 'next-auth/react'
-import { ReactNode } from 'react';
-import { Metadata } from 'next';
-
-interface IProps {
-  children: ReactNode;
-  session: Session
-}
-
-export const metadata: Metadata = {
-  title: 'TrekDiaries',
-  description: 'Social media app for hikers and trekkers',
-}
-// export default function RootLayout({ children }: { children: ReactNode}) {
-export default function RootLayout({ children, session }: IProps) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang="en">
-      <body>
-        <SessionProvider session={ session }>
-          { children }
-        </SessionProvider>
-      </body>
+    <html>
+      <head></head>
+      <body>{children}</body>
     </html>
-  )
+  );
 }
