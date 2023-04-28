63fd5d4a7ea6cf2ee3ff401682ad627b4d766ab3
Re-Dei <hridaya.pradhan123@gmail.com>
2023-04-28 15:01:31 +0545

    separated file structure

---
+++ front-end/src/app/(site)/layout.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { Session } from 'next-auth';
+import './globals.css'
+import { SessionProvider } from 'next-auth/react'
+import { ReactNode } from 'react';
+import { Metadata } from 'next';
+
+interface IProps {
+  children: ReactNode;
+  session: Session
+}
+
+export const metadata: Metadata = {
+  title: 'TrekDiaries',
+  description: 'Social media app for hikers and trekkers',
+}
+// export default function RootLayout({ children }: { children: ReactNode}) {
+export default function RootLayout({ children, session }: IProps) {
+  return (
+    <html lang="en">
+      <body>
+        <SessionProvider session={ session }>
+          { children }
+        </SessionProvider>
+      </body>
+    </html>
+  )
+}
