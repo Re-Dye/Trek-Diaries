f7a2ee1856b19376aff7b92e16e65372547203b9
Re-Dei <hridaya.pradhan123@gmail.com>
2023-04-28 13:48:52 +0545

    fixed fetch call for search

--- front-end/src/app/search/page.tsx
+++ front-end/src/app/search/page.tsx
@@ -9,10 +9,27 @@ import React, { Suspense, useEffect, useState } from 'react'
 import Loading from "./loading"
 import Location from "./components/location/location";
 
-async function fetchLoactions(searchQuery: string) {
+interface Location {
+    _id: string;
+    address: string;
+    description: string;
+}
+
+/**
+ * 
+ * @param searchQuery string to search in database
+ * @param page page number of result
+ * @param searchTime time the search page loaded
+ * @returns Response
+ */
+async function fetchLoactions(searchQuery: string, page: number, searchTime: Date) {
     const encodedQuery = encodeURI(searchQuery)
-    const res: any = await fetch(`https://ap-south-1.aws.data.mongodb-api.com/app/trek-diaries-bmymy/endpoint/searchLocation?location=${ encodedQuery }`,
-    { cache: "no-store" })
+    const encodedPage = encodeURI(page.toString())
+    const encodedSearchTime = encodeURI(searchTime.toISOString())
+    const res: Response = await fetch(
+        `https://ap-south-1.aws.data.mongodb-api.com/app/trek-diaries-bmymy/endpoint/searchLocation?location=${ encodedQuery }&page=${ encodedPage }&searchTime=${ encodedSearchTime }`,
+        { cache: "no-store" }
+    )
     return res.json()
 }
 
@@ -20,15 +37,22 @@ async function fetchLoactions(searchQuery: string) {
 export default function SearchPage() {
     const searchParams = useSearchParams()
     const searchQuery = searchParams? searchParams?.get('q') : null
-    const [locations, setLocations] = useState<any>()
+    const [page, setPage] = useState<number>(0)
+    const [locations, setLocations] = useState<Array<Location>>([])
+    const [searchTime, setSearchTime] = useState<Date>(new Date())
 
     useEffect(() => {
-        const fetchData = async() => {
-            const locations = await fetchLoactions(searchQuery as string)
-            console.log(locations)
-            setLocations(locations)
+        try{
+            const fetchData = async() => {
+                const locations: Array<Location> = await fetchLoactions(searchQuery as string, page, searchTime)
+                console.log(locations)
+                setLocations(locations)
+            }
+            fetchData()
+        }catch(error){
+            console.error(error)
+            alert(error)
         }
-        fetchData()
     }, [])
     
 
