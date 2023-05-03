4bf948c5ce2a60e62752f7014c4e14a70749fbfb
BigyanByanju <90769738+BigyanByanju@users.noreply.github.com>
2023-05-03 08:15:31 +0545

    Layout fixed

--- front-end/src/app/(site)/location/[id]/page.tsx
+++ front-end/src/app/(site)/location/[id]/page.tsx
@@ -3,7 +3,6 @@ import ButtonFollow from "./components/ButtonFollow";
 import ButtonAddPost from "./components/ButtonAddPost";
 import Posts from "./components/Posts";
 
-
 async function fetchLocationData(id: string) {
   const res: any = await fetch(
     `https://ap-south-1.aws.data.mongodb-api.com/app/trek-diaries-bmymy/endpoint/fetchLocaitonData?id=${id}`,
@@ -12,24 +11,37 @@ async function fetchLocationData(id: string) {
   return res.json();
 }
 
-export default async function LocationPage({ params }: {  params: { id: string };}) {
+export default async function LocationPage({
+  params,
+}: {
+  params: { id: string };
+}) {
   const locationID: string = params.id;
   const data = await fetchLocationData(locationID);
 
   return (
-    <div className={locateStyle.location}>
-      <div className={locateStyle.locatefield}>
-        <div className={locateStyle.header}>
-          <div>
-            <h1>{data.address}</h1>
-            <h1>{data.description}</h1>
+    <div className={locateStyle.wrapper}>
+      <div className={locateStyle.left}></div>
+      <div className={locateStyle.center}>
+        <div className={locateStyle.cTop}>
+          <div className={locateStyle.locatefield}>
+            <div className={locateStyle.header}>
+              <div>
+                <h1>{data.address}</h1>
+                <h1>{data.description}</h1>
+              </div>
+            </div>
+            <ButtonAddPost locationID={locationID} />
+            <ButtonFollow locationID={locationID} />
           </div>
         </div>
-        <ButtonAddPost locationID={locationID} />
-        <ButtonFollow locationID={locationID} />
+
+        <div className={locateStyle.cBottom}>
+          <Posts locationId={params.id} />
+        </div>
       </div>
-      <Posts locationId = {params.id}/>
+
+      <div className={locateStyle.left}></div>
     </div>
   );
-} 
-
+}
--- front-end/src/app/(site)/search/page.tsx
+++ front-end/src/app/(site)/search/page.tsx
@@ -1,135 +1,153 @@
-'use client'
+"use client";
 
-import { useSearchParams } from "next/navigation"
+import { useSearchParams } from "next/navigation";
 
 import AddLocation from "./components/modal/AddLocation";
 
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-import Loading from "./loading"
-import ResultLocation from "./components/location/ResultLocation"
+import Loading from "./loading";
+import ResultLocation from "./components/location/ResultLocation";
+import searchStyle from "./page.module.css";
 
 interface Location {
-    _id: string;
-    address: string;
-    description: string;
+  _id: string;
+  address: string;
+  description: string;
 }
 
-const LOCATIONS_PER_SCROLL = 7
+const LOCATIONS_PER_SCROLL = 7;
 
 /**
- * 
+ *
  * @param searchQuery string to search in database
  * @param page page number of result
  * @param searchTime time the search page loaded
  * @returns Response
  */
-async function fetchLocations(searchQuery: string, page: number, searchTime: Date) {
-    const encodedQuery = encodeURI(searchQuery)
-    const encodedPage = encodeURI(page.toString())
-    const encodedSearchTime = encodeURI(searchTime.toISOString())
-    const res: Response = await fetch(
-        `https://ap-south-1.aws.data.mongodb-api.com/app/trek-diaries-bmymy/endpoint/searchLocation?location=${ encodedQuery }&page=${ encodedPage }&searchTime=${ encodedSearchTime }`,
-        { cache: "no-store" }
-    )
-    return res.json()
+async function fetchLocations(
+  searchQuery: string,
+  page: number,
+  searchTime: Date
+) {
+  const encodedQuery = encodeURI(searchQuery);
+  const encodedPage = encodeURI(page.toString());
+  const encodedSearchTime = encodeURI(searchTime.toISOString());
+  const res: Response = await fetch(
+    `https://ap-south-1.aws.data.mongodb-api.com/app/trek-diaries-bmymy/endpoint/searchLocation?location=${encodedQuery}&page=${encodedPage}&searchTime=${encodedSearchTime}`,
+    { cache: "no-store" }
+  );
+  return res.json();
 }
-
 
 export default function SearchPage() {
-    const searchParams = useSearchParams()
-    const searchQuery = searchParams? searchParams?.get('q') : ""
-    const [locations, fetchData, hasMore, didMount] = useFetchLocations(searchQuery as string)
+  const searchParams = useSearchParams();
+  const searchQuery = searchParams ? searchParams?.get("q") : "";
+  const [locations, fetchData, hasMore, didMount] = useFetchLocations(
+    searchQuery as string
+  );
 
-    return (
-        <div>
-            <AddLocation />
-            {didMount && <>
-                    {(locations.length) &&
-                        <InfiniteScroll
-                            dataLength={ locations.length } //This is important field to render the next data
-                            next={ fetchData as any }
-                            hasMore={ hasMore }
-                            loader={<Loading />}
-                            endMessage={
-                                <p style={{ textAlign: 'center' }}>
-                                <b>Yay! You have seen it all</b>
-                                </p>
-                            }
-                        >
-                            {
-                                locations.map((location) => (
-                                    <ResultLocation
-                                        key={ location._id }
-                                        id={ location._id }
-                                        address={ location.address }
-                                        description={ location.description }
-                                    />
-                                ))
-                            }
-                        </InfiniteScroll>
-                    }
-                    {!(locations.length) && <h1>Not Found!</h1>}
-                </>
-            }
-            {!didMount && <Loading/>} 
-        </div>
-    )
+  return (
+    <div className={searchStyle.wrapper}>
+      <div className={searchStyle.left}></div>
+      <div className={searchStyle.center}>
+        <AddLocation />
+        {didMount && (
+          <div className={searchStyle.search}>
+            {locations.length && (
+              <InfiniteScroll
+                dataLength={locations.length} //This is important field to render the next data
+                next={fetchData as any}
+                hasMore={hasMore}
+                loader={<Loading />}
+                endMessage={
+                  <p style={{ textAlign: "center" }}>
+                    <b>Yay! You have seen it all</b>
+                  </p>
+                }
+              >
+                {locations.map((location) => (
+                  <ResultLocation
+                    key={location._id}
+                    id={location._id}
+                    address={location.address}
+                    description={location.description}
+                  />
+                ))}
+              </InfiniteScroll>
+            )}
+            {!locations.length && <h1>Not Found!</h1>}
+          </div>
+        )}
+        {!didMount && <Loading />}
+      </div>
+      <div className={searchStyle.right}></div>
+    </div>
+  );
 }
 
-
-function useFetchLocations(searchQuery: string): [
-    locations: Array<Location>,
-    fetchData: Function,
-    hasMore: boolean,
-    didMount: boolean
+function useFetchLocations(
+  searchQuery: string
+): [
+  locations: Array<Location>,
+  fetchData: Function,
+  hasMore: boolean,
+  didMount: boolean
 ] {
-    const page = useRef<number>(0)
-    const [locations, setLocations] = useState<Array<Location>>([])
-    const searchTime = useRef<Date>(new Date())
-    const [hasMore, setHasMore] = useState<boolean>(true)
-    const [didMount, setDidMount] = useState<boolean>(false)
+  const page = useRef<number>(0);
+  const [locations, setLocations] = useState<Array<Location>>([]);
+  const searchTime = useRef<Date>(new Date());
+  const [hasMore, setHasMore] = useState<boolean>(true);
+  const [didMount, setDidMount] = useState<boolean>(false);
 
-    const fetchData = async() => {
-        console.log("Fetch called")
-        try{
-            /* fetch more locations */
-            const fetchedLocations: Array<Location> = await fetchLocations(searchQuery as string, page.current, searchTime.current)
-            
-            /* add the locations to the existing locations */
-            setLocations((locations) => [...locations, ...fetchedLocations])
+  const fetchData = async () => {
+    console.log("Fetch called");
+    try {
+      /* fetch more locations */
+      const fetchedLocations: Array<Location> = await fetchLocations(
+        searchQuery as string,
+        page.current,
+        searchTime.current
+      );
 
-            /* update page and has more */
-            page.current = page.current + 1
-            setHasMore(!(fetchedLocations.length < LOCATIONS_PER_SCROLL))
-        }catch(error){
-            console.error(error)
-            alert(error)
-        }
+      /* add the locations to the existing locations */
+      setLocations((locations) => [...locations, ...fetchedLocations]);
+
+      /* update page and has more */
+      page.current = page.current + 1;
+      setHasMore(!(fetchedLocations.length < LOCATIONS_PER_SCROLL));
+    } catch (error) {
+      console.error(error);
+      alert(error);
     }
+  };
 
-    /* fetch data once on render */
-    useEffect(() => {
-        try{
-            const fetchData = async() => {
-                /* fetch more locations */
-                const fetchedLocations: Array<Location> = await fetchLocations(searchQuery as string, page.current, searchTime.current)
-                
-                /* add the locations to the existing locations */
-                setLocations(fetchedLocations)
-    
-                /* update page and has more */
-                page.current = 1
-                setHasMore(!(fetchedLocations.length < LOCATIONS_PER_SCROLL))
-                setDidMount(true)
-            }
-            fetchData()
-        }catch(error){
-            alert(error)
-            console.error(error)
-        }
-    }, [searchQuery])
+  /* fetch data once on render */
+  useEffect(() => {
+    try {
+      const fetchData = async () => {
+        /* fetch more locations */
+        const fetchedLocations: Array<Location> = await fetchLocations(
+          searchQuery as string,
+          page.current,
+          searchTime.current
+        );
 
-    return [locations, fetchData, hasMore, didMount]
+        /* add the locations to the existing locations */
+        setLocations(fetchedLocations);
+
+        /* update page and has more */
+        page.current = 1;
+        setHasMore(!(fetchedLocations.length < LOCATIONS_PER_SCROLL));
+        setDidMount(true);
+      };
+      fetchData();
+    } catch (error) {
+      alert(error);
+      console.error(error);
+    }
+  }, [searchQuery]);
+
+  return [locations, fetchData, hasMore, didMount];
 }
