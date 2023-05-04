464409ea8c2795fbc718e4d03df0308a9cff8e37
Re-Dei <hridaya.pradhan123@gmail.com>
2023-05-04 14:34:31 +0545

    made posts in home page

---
+++ front-end/src/app/(site)/components/PostFeed/PostFeed.tsx
@@ -0,0 +1,138 @@
+'use client'
+import ViewPost from "../viewPost/viewPost";
+import { useRef, useState, useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+
+interface Post {
+    _id: string;
+    address: string;
+    description: string;
+    imageURL: string;
+    likes: number;
+    location: any;
+    pictureURL: string;
+    owner: any;
+  }
+
+const POSTS_PER_SCROLL = 7;
+
+async function fetchLocationPosts(
+    email: string,
+    page: number,
+    searchTime: Date
+  ) {
+    const encodedEmail: any = encodeURI(email);
+    const encodedPage: string = encodeURI(page.toString());
+    const encodedSearchTime: any = encodeURI(searchTime.toISOString());
+    const res: any = await fetch(
+      `https://ap-south-1.aws.data.mongodb-api.com/app/trek-diaries-bmymy/endpoint/getPostFeed?email=${ encodedEmail }&page=${ encodedPage }&searchTime=${ encodedSearchTime }`,
+      { cache: "no-store" }
+    );
+    return res.json();
+  }
+
+export default function PostFeed({ email }:{ email: string }) {
+    const [posts, fetchPosts, hasMore, didMount] = useFetchPosts(email);
+
+    return(
+        <div className="PostBody">
+            {didMount && <>
+                    {(posts.length) &&
+                        <InfiniteScroll
+                            dataLength={ posts.length } //This is important field to render the next data
+                            next={ fetchPosts as any }
+                            hasMore={ hasMore }
+                            loader={<p>Loading...</p>}
+                            endMessage={
+                              <p style={{ textAlign: 'center' }}>
+                              <b>Yay! You have seen it all</b>
+                              </p>
+                            }
+                        >
+                            {
+                                posts.map((post) => (
+                                    <ViewPost
+                                        key={ post._id }
+                                        id={ post._id }
+                                        location={ post.location }
+                                        description={ post.description }
+                                        likes = {post.likes}
+                                        imageURL = {post.pictureURL}
+                                        owner = {post.owner}
+                                    />
+                                ))
+                            }
+                        </InfiniteScroll>
+                    }
+                    {!(posts.length) && <h1>Not Found!</h1>}
+                </>
+            }
+            {!didMount} 
+        </div>
+    )
+    
+}
+
+function useFetchPosts( email: string ):[
+    posts: Array<Post>,
+    fetchPost: Function,
+    hasMore: boolean,
+    didMount: boolean
+  ] {
+    const page = useRef<number>(0);
+    const searchTime = useRef<Date>(new Date());
+    const [posts, setPosts] = useState<Array<Post>>([]);
+    const [hasMore, setHasMore] = useState<boolean>(true);
+    const [didMount, setDidMount] = useState<boolean>(false);
+  
+    const fetchPosts = async () => {
+      console.log("Fetch called");
+      try {
+        /* fetch more posts */
+        const fetchedPosts: Array<any> = await fetchLocationPosts(
+          email as string,
+          page.current,
+          searchTime.current
+        );
+
+        /* add the locations to the existing locations */
+        setPosts((posts) => [...posts, ...fetchedPosts]);
+  
+        /* update page and has more */
+        page.current = page.current + 1;
+        setHasMore(!(fetchedPosts.length < POSTS_PER_SCROLL))
+      } catch (error) {
+        console.error(error); 
+        alert(error);
+      }
+    }
+
+      /* fetch data on the render */
+      useEffect(() => {
+        if (!didMount) {
+            try{
+                const fetchPost = async() => {
+                    /* fetch more posts */
+                    const fetchedPosts: Array<any> = await fetchLocationPosts(email as string, page.current, searchTime.current)
+
+                    console.log(fetchedPosts);
+                    /* add the locations to the existing locations */
+                    setPosts(fetchedPosts);
+        
+                    /* update page and has more */
+                    page.current = 1
+                    setHasMore(!(fetchedPosts.length < POSTS_PER_SCROLL))
+                    setDidMount(true)
+                }
+                fetchPost()
+            }catch(error){
+                alert(error)
+                console.error(error)
+            }
+          }
+        }, [])
+    return  [posts, fetchPosts, hasMore, didMount];
+    };
+  
+  
--- front-end/src/app/(site)/page.tsx
+++ front-end/src/app/(site)/page.tsx
@@ -4,6 +4,7 @@ import { useSession } from "next-auth/react";
 import { useEffect } from "react";
 import mainStyles from "./page.module.css";
 import ViewPost from "./components/viewPost/viewPost";
+import PostFeed from "./components/PostFeed/PostFeed";
 
 export default function Home() {
   const router = useRouter();
@@ -18,7 +19,9 @@ export default function Home() {
       <div className={mainStyles.wrapper}>
         <div className={mainStyles.left}></div>
         <div className={mainStyles.center}>
-          {/* <ViewPost /> */}
+          {(session.status === "authenticated") &&
+            <PostFeed email={ session.data?.user?.email as string }/>
+          }
         </div>
         <div className={mainStyles.right}></div>
       </div>
