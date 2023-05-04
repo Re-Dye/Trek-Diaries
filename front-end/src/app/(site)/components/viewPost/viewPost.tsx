e8a7813cf0e2745fb582d41a7a49bdb40bdb2614
BigyanByanju <90769738+BigyanByanju@users.noreply.github.com>
2023-05-04 16:22:13 +0545

    post layout fixed

--- front-end/src/app/(site)/components/viewPost/viewPost.tsx
+++ front-end/src/app/(site)/components/viewPost/viewPost.tsx
@@ -1,38 +1,49 @@
 import postStyles from "./page.module.css";
 import { AiTwotoneLike } from "react-icons/ai";
 import { FaCommentAlt } from "react-icons/fa";
-import Image from "next/image"
-import { useRouter } from "next/navigation"
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 interface Owner {
-  name: string,
+  name: string;
 }
 
 //{id, location, description, likes, imageURL, owner}
-export default function ViewPost({ id, location, description, likes, imageURL, owner }: {
-  id: string,
-  location: any,
-  description: string,
-  likes: number,
-  imageURL: string,
-  owner: Owner
+export default function ViewPost({
+  id,
+  location,
+  description,
+  likes,
+  imageURL,
+  owner,
+}: {
+  id: string;
+  location: any;
+  description: string;
+  likes: number;
+  imageURL: string;
+  owner: Owner;
 }) {
-
   const router = useRouter();
-  const handleComment = ()=>{
+  const handleComment = () => {
     router.push(`/post/${id}`);
-  }
+  };
 
   return (
     <div className={postStyles.wrapper}>
       <div className={postStyles.left}>
-        <h3>{location.address}</h3>
-        <div className={postStyles.imgCtn}>
-          <Image 
-          alt="Error: Image could not be loaded."
-          width = '1000'
-          height = '500'
-          src = {imageURL}/>
+        <div className={postStyles.Tleft}>
+          <h3>{location.address}</h3>
+        </div>
+        <div className={postStyles.Bleft}>
+          <div className={postStyles.imgCtn}>
+            <Image
+              alt="Error: Image could not be loaded."
+              width="1000"
+              height="500"
+              src={imageURL}
+            />
+          </div>
         </div>
       </div>
       <div className={postStyles.right}>
@@ -54,7 +65,10 @@ export default function ViewPost({ id, location, description, likes, imageURL, o
           <button
             className={`${postStyles.icons} ${postStyles.like}`}
             // size={35}
-          > LIKE </button>
+          >
+            {" "}
+            LIKE{" "}
+          </button>
           {/* <FaCommentAlt
             className={`${postStyles.icons} ${postStyles.comment}`}
             size={28}
@@ -62,8 +76,11 @@ export default function ViewPost({ id, location, description, likes, imageURL, o
           <button
             className={`${postStyles.icons} ${postStyles.comment}`}
             // size={28}
-            onClick = {handleComment}
-          > COMMENT </button>
+            onClick={handleComment}
+          >
+            {" "}
+            COMMENT{" "}
+          </button>
         </div>
       </div>
     </div>
