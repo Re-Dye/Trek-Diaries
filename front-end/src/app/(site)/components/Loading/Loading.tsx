6b27ee0627299ca0bad0e187f53dac9006469639
BigyanByanju <90769738+BigyanByanju@users.noreply.github.com>
2023-05-24 21:32:29 +0545

    skeleton

---
+++ front-end/src/app/(site)/components/Loading/Loading.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Loading.css";
+
+const Loading = () => {
+  return (
+    <div className="wrapper skeleton">
+      <div className="left">
+        <div className="Tleft skeleton">
+          <div className="skeleton skeleton-text"></div>
+        </div>
+        <div className="Bleft">
+          <div className="imgCtn skeleton"></div>
+        </div>
+      </div>
+      <div className="right">
+        <div className="rTop skeleton">
+          <div className="uName skeleton skeleton-text"></div>
+          <div className="time skeleton skeleton-text"></div>
+        </div>
+        <div className="rCenter skeleton">
+          <div className="skeleton skeleton-text"></div>
+          <div className="skeleton skeleton-text"></div>
+          <div className="skeleton skeleton-text"></div>
+        </div>
+        <div className="rBottom skeleton "></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
