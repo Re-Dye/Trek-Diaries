8213012227779e51055ea743a2b2496df4476166
BigyanByanju <90769738+BigyanByanju@users.noreply.github.com>
2023-05-24 13:08:19 +0545

    not found done

--- front-end/src/app/not-found.tsx
+++ front-end/src/app/not-found.tsx
@@ -1,10 +1,21 @@
 "use client"
+import Image from 'next/image';
 
 export default function NotFound() {
+  const imageStyle = {
+    display:"flex",
+    justifyContent:"center",
+    marginTop:"20%"
+
+  };
     return (
-      <>
-        <h2>Not Found</h2>
-        <p>Could not find requested resource</p>
-      </>
+      <div style={imageStyle}>
+        <Image 
+        src="/not_found.png"
+        width={500}
+        height={403.44}
+        alt="Not Found"
+        />
+      </div>
     );
 }
