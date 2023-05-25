6533a7c26ee758b80a0e255014ab47753a5b1fd3
goofysoul <manishsunuwar4@gmail.com>
2023-05-25 12:46:09 +0545

    60% of reset

--- front-end/src/app/(auth)/reset-password/page.tsx
+++ front-end/src/app/(auth)/reset-password/page.tsx
@@ -1,36 +1,59 @@
-'use client'
-import React, { useState } from "react"
+"use client";
+import React, { useState } from "react";
 import forgetStyles from "../reset-password/page.module.css";
 import Image from "next/image";
 import Link from "next/link";
+import axios from "axios";
 
-export default function ResetPassword()
-{
-    const [email, setEmail] = useState("");
-    const [loading, setLoading] = useState(false);
+export default function ResetPassword() {
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
 
-    const handleForgot = () => {}
+  const handleForgot = async (e:React.FormEvent) => {
+    e.preventDefault(); // Prevent form submission
+    console.log(email);
+    try {
+      const { data } = await axios.post("/api/forgot_password", {
+        email
+      });
 
-    return (
+      console.log(data);
+      alert(`Verification email sent to: ${email}`);
+
+      setEmail("")
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
     <div className={forgetStyles.wrapper}>
-        <Image className={forgetStyles.img} src="/ncpr.jpg" alt="backgroundImage" fill  />
+      <Image
+        className={forgetStyles.img}
+        src="/ncpr.jpg"
+        alt="backgroundImage"
+        fill
+      />
       <form onSubmit={handleForgot} className={forgetStyles.forgetBox}>
         <h1>Forgot Your Password ?</h1>
         <p>Please enter the email you use to sign in to Trek-Diaries</p>
-          <input
-            className={forgetStyles.inpforget}
-            type= "email"
-            id= "email"
-            placeholder="your email address"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        <button className={forgetStyles.resetbtn}>
-          {!loading ? 'Request Password Reset': 'Sending...'}
+        <input
+          className={forgetStyles.inpforget}
+          type="email"
+          id="email"
+          placeholder="Enter your email address"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <button type="submit" className={forgetStyles.resetbtn}>
+          {/* {!loading ? 'Request Password Reset': 'Sending...'} */}
         </button>
-        <Link className={forgetStyles.linkforget} href='/login'>Back to sign in</Link>
+        <Link className={forgetStyles.linkforget} href="/login">
+          Back to sign in
+        </Link>
       </form>
     </div>
-  )
+  );
 }
