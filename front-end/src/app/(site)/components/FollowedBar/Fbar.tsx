d0323801c0a0c6c194319bd7f36b414eb621d55d
Shashanka <71580178+Shashanka10@users.noreply.github.com>
2023-05-25 20:09:00 +0545

    some css fixed

--- front-end/src/app/(site)/components/FollowedBar/Fbar.tsx
+++ front-end/src/app/(site)/components/FollowedBar/Fbar.tsx
@@ -7,7 +7,7 @@ import { Location } from "../../layout";
 export default function Fbar({ locations }: { locations: Array<Location> }) {
     return(
         <div className={followStyle.followbar}>
-            <h2>Followed Locations </h2>
+            <h2>Followed Locations <ImLocation2 className={followStyle.locicon}/></h2>
             <div className={followStyle.followLocation}>
             {
                 locations.map((location) => (
