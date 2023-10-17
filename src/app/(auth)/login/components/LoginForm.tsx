067231cd4298a7160bbbcfba933775c374e7ba17
Re-Dei <hridaya.pradhan123@gmail.com>
2023-10-17 18:34:32 +0545

    Update LoginForm.tsx

--- src/app/(auth)/login/components/LoginForm.tsx
+++ src/app/(auth)/login/components/LoginForm.tsx
@@ -6,7 +6,6 @@ import Link from "next/link";
 import Image from "next/image";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import * as z from "zod";
 import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
@@ -20,7 +19,7 @@ export default function Login() {
   const router = useRouter();
   const [showPassword, setShowPassword] = useState<boolean>(false);
 
-  const form = useForm<z.infer<typeof loginSchema>>({
+  const form = useForm<LoginFormData>({
     resolver: zodResolver(loginSchema),
   })
 
