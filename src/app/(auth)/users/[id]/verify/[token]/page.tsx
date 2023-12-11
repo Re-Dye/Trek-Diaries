35284ca9e53e36576ccb0418343e0a803bd78a3c
Shashanka <71580178+Shashanka10@users.noreply.github.com>
2023-12-11 16:00:30 +0545

    ui updated and preference page added

--- src/app/(auth)/users/[id]/verify/[token]/page.tsx
+++ src/app/(auth)/users/[id]/verify/[token]/page.tsx
@@ -61,12 +61,12 @@ export default async function UserVerifyPage({
   const message = await verifyEmail({ id: params.id, token: params.token });
 
   return (
-    <div className="fixed flex w-full h-screen justify-center items-center self-center">
-      <Card className="justify-center flex-row space-y-2 items-center absolute w-4/5 h-3/5 rounded-xl p-4 sm:w-3/5 sm:p-6 md:rounded-2xl md:p-8 lg: ">
+    <div className="relative rounded-xl m-4 z-10 sm:p-2 sm:w-full sm:m-2 md:m-3 lg:m-4 xl:m-6">
+      <Card className="flex-row sm:p-2">
         <div className="flex absolute top-5 right-5 sm:top-7 sm:right-7 md:top-9 md:right-9">
           <ModeToggle />
         </div>
-        <CardHeader className="flex justify-center items-center mt-2 sm:mt-6 md:mt-8">
+        <CardHeader className="flex justify-center items-center mt-6 sm:mt-6 md:mt-8">
           <CardTitle>
             <MailCheck className="w-8 h-8 text-lime-600 sm:w-12 sm:h-12 md:w-14 md:h-14" />
           </CardTitle>
@@ -77,10 +77,10 @@ export default async function UserVerifyPage({
         <CardContent>
           <div className=" flex items-center rounded-md border p-4 sm:p-5 md:p-5">
             <div className="flex-1 space-y-4 sm:space-y-5">
-              <h1 className="text-sm font-medium leading-none sm:text-md md:text-lg">
+              <h1 className="text-md font-medium leading-none sm:text-lg">
                 {message}
               </h1>
-              <p className=" text-xs text-muted-foreground sm:text-sm md:text-md">
+              <p className=" text-sm text-muted-foreground sm:text-md md:text-lg">
                 Click on continue button
               </p>
             </div>
---
+++ src/app/(site)/Preferences/components/Preferences.tsx
@@ -0,0 +1,335 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+import { toast } from "@/components/ui/use-toast"
+import { Focus } from "lucide-react"
+
+const FormSchema = z.object({
+  type: z.enum(["all", "mentions", "none"], {
+    required_error: "You need to select a notification type.",
+  }),
+})
+
+
+export default function Preferences() {
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+      })
+    
+      function onSubmit(data: z.infer<typeof FormSchema>) {
+        toast({
+          title: "You submitted the following values:",
+          description: (
+            <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+              <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+            </pre>
+          ),
+        })
+      }
+    
+      return (
+        <div className=" space-y-4">
+            <div className="flex justify-center gap-3">
+                <h1 className=" text-xl tracking-wider font-semibold">My Preferences</h1><Focus className="text-teal-300"/>
+            </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-5">
+            <FormField
+              control={form.control}
+              name="type"
+              render={({ field }) => (
+                <FormItem className="space-y-5 bg-black p-6 rounded-2xl">
+                  <FormLabel className="flex justify-start">Trekking Expertise</FormLabel>
+                  <FormControl>
+                    <RadioGroup
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                      className="flex flex-col space-y-1"
+                    >
+                      <FormItem className="flex items-center space-x-3 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="all" />
+                        </FormControl>
+                        <FormLabel className="font-normal">
+                          Beginner
+                        </FormLabel>
+                      </FormItem>
+                      <FormItem className="flex items-center space-x-3 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="mentions" />
+                        </FormControl>
+                        <FormLabel className="font-normal">
+                          Average
+                        </FormLabel>
+                      </FormItem>
+                      <FormItem className="flex items-center space-x-3 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="none" />
+                        </FormControl>
+                        <FormLabel className="font-normal">Expertise</FormLabel>
+                      </FormItem>
+                    </RadioGroup>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="type"
+              render={({ field }) => (
+                <FormItem className="space-y-5 bg-black p-6 rounded-2xl">
+                  <FormLabel className="flex justify-start">Interested Trails </FormLabel>
+                  <FormControl>
+                    <RadioGroup
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                      className="flex flex-col space-y-1"
+                    >
+                      <FormItem className="flex items-center space-x-3 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="all" />
+                        </FormControl>
+                        <FormLabel className="font-normal">
+                          All new messages
+                        </FormLabel>
+                      </FormItem>
+                      <FormItem className="flex items-center space-x-3 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="mentions" />
+                        </FormControl>
+                        <FormLabel className="font-normal">
+                          Direct messages and mentions
+                        </FormLabel>
+                      </FormItem>
+                      <FormItem className="flex items-center space-x-3 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="none" />
+                        </FormControl>
+                        <FormLabel className="font-normal">Nothing</FormLabel>
+                      </FormItem>
+                    </RadioGroup>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="type"
+              render={({ field }) => (
+                <FormItem className="space-y-5 bg-black p-6 rounded-2xl">
+                  <FormLabel className="flex justify-start">Interested Locations</FormLabel>
+                  <FormControl>
+                    <RadioGroup
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                      className="flex flex-col space-y-1"
+                    >
+                      <FormItem className="flex items-center space-x-3 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="all" />
+                        </FormControl>
+                        <FormLabel className="font-normal">
+                          All new messages
+                        </FormLabel>
+                      </FormItem>
+                      <FormItem className="flex items-center space-x-3 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="mentions" />
+                        </FormControl>
+                        <FormLabel className="font-normal">
+                          Direct messages and mentions
+                        </FormLabel>
+                      </FormItem>
+                      <FormItem className="flex items-center space-x-3 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="none" />
+                        </FormControl>
+                        <FormLabel className="font-normal">Nothing</FormLabel>
+                      </FormItem>
+                    </RadioGroup>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="type"
+              render={({ field }) => (
+                <FormItem className="space-y-5 bg-black p-6 rounded-2xl">
+                  <FormLabel className="flex justify-start">Preferred Trekking Distance</FormLabel>
+                  <FormControl>
+                    <RadioGroup
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                      className="flex flex-col space-y-1"
+                    >
+                      <FormItem className="flex items-center space-x-3 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="all" />
+                        </FormControl>
+                        <FormLabel className="font-normal">
+                          All new messages
+                        </FormLabel>
+                      </FormItem>
+                      <FormItem className="flex items-center space-x-3 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="mentions" />
+                        </FormControl>
+                        <FormLabel className="font-normal">
+                          Direct messages and mentions
+                        </FormLabel>
+                      </FormItem>
+                      <FormItem className="flex items-center space-x-3 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="none" />
+                        </FormControl>
+                        <FormLabel className="font-normal">Nothing</FormLabel>
+                      </FormItem>
+                    </RadioGroup>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="type"
+              render={({ field }) => (
+                <FormItem className="space-y-5 bg-black p-6 rounded-2xl">
+                  <FormLabel className="flex justify-start">Preferred Trekking Altitude</FormLabel>
+                  <FormControl>
+                    <RadioGroup
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                      className="flex flex-col space-y-1"
+                    >
+                      <FormItem className="flex items-center space-x-3 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="all" />
+                        </FormControl>
+                        <FormLabel className="font-normal">
+                          All new messages
+                        </FormLabel>
+                      </FormItem>
+                      <FormItem className="flex items-center space-x-3 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="mentions" />
+                        </FormControl>
+                        <FormLabel className="font-normal">
+                          Direct messages and mentions
+                        </FormLabel>
+                      </FormItem>
+                      <FormItem className="flex items-center space-x-3 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="none" />
+                        </FormControl>
+                        <FormLabel className="font-normal">Nothing</FormLabel>
+                      </FormItem>
+                    </RadioGroup>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="type"
+              render={({ field }) => (
+                <FormItem className="space-y-5 bg-black p-6 rounded-2xl">
+                  <FormLabel className="flex justify-start">Preferred trekking month</FormLabel>
+                  <FormControl>
+                    <RadioGroup
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                      className="flex flex-col space-y-1"
+                    >
+                      <FormItem className="flex items-center space-x-3 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="all" />
+                        </FormControl>
+                        <FormLabel className="font-normal">
+                          All new messages
+                        </FormLabel>
+                      </FormItem>
+                      <FormItem className="flex items-center space-x-3 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="mentions" />
+                        </FormControl>
+                        <FormLabel className="font-normal">
+                          Direct messages and mentions
+                        </FormLabel>
+                      </FormItem>
+                      <FormItem className="flex items-center space-x-3 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="none" />
+                        </FormControl>
+                        <FormLabel className="font-normal">Nothing</FormLabel>
+                      </FormItem>
+                    </RadioGroup>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="type"
+              render={({ field }) => (
+                <FormItem className="space-y-5 bg-black p-6 rounded-2xl">
+                  <FormLabel className="flex justify-start">Any Other Interests</FormLabel>
+                  <FormControl>
+                    <RadioGroup
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                      className="flex flex-col space-y-1"
+                    >
+                      <FormItem className="flex items-center space-x-3 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="all" />
+                        </FormControl>
+                        <FormLabel className="font-normal">
+                          All new messages
+                        </FormLabel>
+                      </FormItem>
+                      <FormItem className="flex items-center space-x-3 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="mentions" />
+                        </FormControl>
+                        <FormLabel className="font-normal">
+                          Direct messages and mentions
+                        </FormLabel>
+                      </FormItem>
+                      <FormItem className="flex items-center space-x-3 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="none" />
+                        </FormControl>
+                        <FormLabel className="font-normal">Nothing</FormLabel>
+                      </FormItem>
+                    </RadioGroup>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+        </div>
+      )
+}
