4854b925085d6ca4b222db30ef495e1c87a48d60
BigyanByanju <90769738+BigyanByanju@users.noreply.github.com>
2023-12-16 09:33:08 +0545

    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ const nextConfig = {
             source: "/api/flask/:path*",
             destination:
               process.env.NODE_ENV === "development"
-                ? "http://127.0.0.1:5000/api/flask/:path*"
+                ? "http://127.0.0.1:5000//:path*"
                 : "/api/flask/",
           },
         ];
