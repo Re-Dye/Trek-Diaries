4221f02cf03d21a413af10b5aaba840be768a284
Re-Dei <hridaya.pradhan123@gmail.com>
2024-02-13 15:28:14 +0545

    refactor: used dotenv for python

--- src/app/api/flask/Recommend.py
+++ src/app/api/flask/Recommend.py
@@ -1,10 +1,7 @@
 import numpy as np
 import pandas as pd
-from sklearn.metrics.pairwise import cosine_similarity
 import difflib
-from flask import request, jsonify, Response
 import os
-import json
 from collections import Counter
 
 # function to get the trail data
--- src/app/api/flask/app.py
+++ src/app/api/flask/app.py
@@ -1,14 +1,19 @@
 
 from flask import Flask
 from Recommend import get_recommendation_locations, get_recommendation_trails
-from flask import request, jsonify, Response
+from flask import request
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+DB_URL = os.getenv('PYTHON_DB_URL')
 
 # from flask_cors import CORS
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://hridaya.pradhan123:c6vIW2xQlNnf@ep-muddy-darkness-92363127-pooler.ap-southeast-1.aws.neon.tech/trek-diaries'
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
 db = SQLAlchemy(app)
 
 
@@ -17,19 +22,6 @@ class Preferences(db.Model):
     __table_args__ = {'extend_existing': True}
     user_id = db.Column(db.String(50), primary_key=True)
     trail = db.Column(db.String(50))
-
-
-# @app.route("/test", methods=['GET'])
-# def test_endpoint():
-#     youtubeLink = request.args.get('youtubeLink')
-#     comment = request.args.get('comment')
-#     model = request.args.get('model')
-#     pageNumber = request.args.get('pageNumber')
-#     return jsonify({'youtubeLink': youtubeLink,
-#                     "comment": comment,
-#                     "model": model,
-#                     "pageNumber": pageNumber
-#                     })
 
 
 @app.route("/trail_recommendation", methods=['GET'])
