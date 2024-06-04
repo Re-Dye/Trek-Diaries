b3e2f484102fe1cd38650e45b236338057794ab7
Re-Dei <hridaya.pradhan123@gmail.com>
2024-06-04 17:34:05 +0545

    chore: address index in location table

---
+++ src/drizzle/0017_cynical_starbolt.sql
@@ -0,0 +1,1 @@
+CREATE UNIQUE INDEX IF NOT EXISTS "address_idx" ON "locations" ("address");
--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -9,118 +9,118 @@ import {
   uuid,
   index,
   pgEnum,
-  json,
-} from "drizzle-orm/pg-core";
-import { CONSTANTS } from "../constants";
+  uniqueIndex
+} from 'drizzle-orm/pg-core';
+import { CONSTANTS } from '../constants';
 
 export const users = pgTable(
-  "users",
+  'users',
   {
-    id: text("id").notNull().primaryKey(),
-    name: text("name").notNull(),
-    email: text("email").notNull(),
-    password: char("password", {
+    id: text('id').notNull().primaryKey(),
+    name: text('name').notNull(),
+    email: text('email').notNull(),
+    password: char('password', {
       length: CONSTANTS.ENCRYPTED_PASSWORD_LENGTH,
     }).notNull(),
-    dob: date("dob", { mode: "string" }).notNull(),
-    image: text("image"),
+    dob: date('dob', { mode: 'string' }).notNull(),
+    image: text('image'),
   },
   (users) => ({
-    emailIdx: index("email_idx").on(users.email),
+    emailIdx: index('email_idx').on(users.email),
   })
 );
 
-export const locations = pgTable("locations", {
-  id: uuid("id").defaultRandom().primaryKey(),
-  address: text("address").notNull().unique(),
-  registered_time: timestamp("registered_time", { mode: "string" })
-    .defaultNow()
-    .notNull(),
-  description: text("description").notNull(),
-});
+export const locations = pgTable(
+  'locations',
+  {
+    id: uuid('id').defaultRandom().primaryKey(),
+    address: text('address').notNull().unique(),
+    registered_time: timestamp('registered_time', { mode: 'string' }).defaultNow().notNull(),
+    description: text('description').notNull(),
+  },
+  (locations) => ({
+    addressIdx: uniqueIndex('address_idx').on(locations.address),
+  })
+);
 
 export const posts = pgTable(
-  "posts",
+  'posts',
   {
-    id: uuid("id").defaultRandom().primaryKey(),
-    registered_time: timestamp("registered_time", { mode: "string" })
-      .defaultNow()
-      .notNull(),
-    description: text("description").notNull(),
-    trail_condition: integer("trail_condition").notNull(),
-    weather: integer("weather").notNull(),
-    accessibility: integer("accessibility").notNull(),
-    picture_url: text("picture_url").notNull(),
-    likes_count: integer("likes_count").notNull().default(0),
-    location_id: uuid("location_id")
+    id: uuid('id').defaultRandom().primaryKey(),
+    registered_time: timestamp('registered_time', { mode: 'string' }).defaultNow().notNull(),
+    description: text('description').notNull(),
+    trail_condition: integer('trail_condition').notNull(),
+    weather: integer('weather').notNull(),
+    accessibility: integer('accessibility').notNull(),
+    picture_url: text('picture_url').notNull(),
+    likes_count: integer('likes_count').notNull().default(0),
+    location_id: uuid('location_id')
       .notNull()
       .references(() => locations.id, {
-        onDelete: "cascade",
+        onDelete: 'cascade',
       }),
-    owner_id: text("owner_id")
+    owner_id: text('owner_id')
       .notNull()
       .references(() => users.id, {
-        onDelete: "cascade",
+        onDelete: 'cascade',
       }),
   },
   (posts) => ({
-    locationIdIdx: index("location_id_idx").on(posts.location_id),
-    ownerIdIdx: index("owner_id_idx").on(posts.owner_id),
+    locationIdIdx: index('location_id_idx').on(posts.location_id),
+    ownerIdIdx: index('owner_id_idx').on(posts.owner_id),
   })
 );
 
 export const comments = pgTable(
-  "comments",
+  'comments',
   {
-    id: uuid("id").defaultRandom().primaryKey(),
-    user_id: text("user_id")
+    id: uuid('id').defaultRandom().primaryKey(),
+    user_id: text('user_id')
       .notNull()
       .references(() => users.id, {
-        onDelete: "cascade",
+        onDelete: 'cascade',
       }),
-    post_id: uuid("post_id")
+    post_id: uuid('post_id')
       .notNull()
       .references(() => posts.id, {
-        onDelete: "cascade",
+        onDelete: 'cascade',
       }),
-    content: text("content").notNull(),
-    registered_time: timestamp("registered_time", { mode: "string" })
-      .defaultNow()
-      .notNull(),
+    content: text('content').notNull(),
+    registered_time: timestamp('registered_time', { mode: 'string' }).defaultNow().notNull(),
   },
   (comments) => ({
-    userIdIdx: index("user_id_idx").on(comments.user_id),
-    postIdIdx: index("post_id_idx").on(comments.post_id),
+    userIdIdx: index('user_id_idx').on(comments.user_id),
+    postIdIdx: index('post_id_idx').on(comments.post_id),
   })
 );
 
 export const usersToLocations = pgTable(
-  "users_to_locations",
+  'users_to_locations',
   {
-    userId: text("user_id")
+    userId: text('user_id')
       .notNull()
-      .references(() => users.id, { onDelete: "cascade" }),
-    locationId: uuid("location_id")
+      .references(() => users.id, { onDelete: 'cascade' }),
+    locationId: uuid('location_id')
       .notNull()
       .references(() => locations.id, {
-        onDelete: "cascade",
+        onDelete: 'cascade',
       }),
   },
   (usersToLocations) => ({
     pk: primaryKey(usersToLocations.userId, usersToLocations.locationId),
-    userIdIdx: index("user_id_idx").on(usersToLocations.userId),
-    locationIdIdx: index("location_id_idx").on(usersToLocations.locationId),
+    userIdIdx: index('user_id_idx').on(usersToLocations.userId),
+    locationIdIdx: index('location_id_idx').on(usersToLocations.locationId),
   })
 );
 
 export const usersLikePosts = pgTable(
-  "users_like_posts",
+  'users_like_posts',
   {
-    user_id: text("user_id").references(() => users.id, {
-      onDelete: "cascade",
+    user_id: text('user_id').references(() => users.id, {
+      onDelete: 'cascade',
     }),
-    post_id: uuid("post_id").references(() => posts.id, {
-      onDelete: "cascade",
+    post_id: uuid('post_id').references(() => posts.id, {
+      onDelete: 'cascade',
     }),
   },
   (usersLikePosts) => ({
@@ -128,31 +128,31 @@ export const usersLikePosts = pgTable(
   })
 );
 
-export const preferenceType = pgEnum("type", ["easy", "moderate", "challenging"]);
-export const month = pgEnum("month", [
-  "jan",
-  "feb",
-  "mar",
-  "apr",
-  "may",
-  "jun",
-  "jul",
-  "aug",
-  "sep",
-  "nov",
-  "dec",
+export const preferenceType = pgEnum('type', ['easy', 'moderate', 'challenging']);
+export const month = pgEnum('month', [
+  'jan',
+  'feb',
+  'mar',
+  'apr',
+  'may',
+  'jun',
+  'jul',
+  'aug',
+  'sep',
+  'nov',
+  'dec',
 ]);
 
-export const preferences = pgTable("preferences", {
-  user_id: text("user_id")
+export const preferences = pgTable('preferences', {
+  user_id: text('user_id')
     .primaryKey()
     .references(() => users.id, {
-      onDelete: "cascade",
+      onDelete: 'cascade',
     }),
-  type: preferenceType("type").notNull(),
-  trail: text("trail").notNull(),
-  distance: integer("distance").notNull(),
-  altitude: integer("altitude").notNull(),
-  month: month("month").notNull(),
-  features: text("features").notNull(),
+  type: preferenceType('type').notNull(),
+  trail: text('trail').notNull(),
+  distance: integer('distance').notNull(),
+  altitude: integer('altitude').notNull(),
+  month: month('month').notNull(),
+  features: text('features').notNull(),
 });
